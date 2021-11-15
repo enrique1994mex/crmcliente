3821f5b9b9d66a0150658a27fb8a166e7848aee3
enrique1994mex <enrique.antonio1029@gmail.com>
2021-11-14 22:28:01 -0600

    Se agregaron todos los cambios

---
+++ components/Layout.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Head from 'next/dist/shared/lib/head';
+import Sidebar from './Sidebar';
+import Header from '../components/Header'; 
+import { useRouter } from 'next/dist/client/router';
+
+const Layout = ({children}) => {
+
+    //Hook de routing
+    const router = useRouter();
+    return(
+        <>
+            <Head>
+                <title>CRM - Administración de Clientes</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <   link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
+            </Head>
+
+            {router.pathname === '/login' || router.pathname === '/nuevaCuenta' ? (
+                <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+                    <div>
+                        {children}
+                    </div>
+                </div>
+            ) : (
+                <div className="bg-gray-200 min-h-screen">
+                    <div className="sm:flex min-h-screen">
+                        <Sidebar/>            
+                        <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                            <Header/> 
+                            {children}
+                        </main>
+                    </div>
+                </div>
+            )}    
+        </>
+    )
+}
+
+export default Layout; 
---
+++ config/apollo.js
@@ -0,0 +1,29 @@
+import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+import fetch from 'cross-fetch'; 
+import { setContext } from 'apollo-link-context';
+
+const httpLink = createHttpLink({
+    uri: 'https://morning-wildwood-16524.herokuapp.com/',
+    fetch  
+})
+
+const authLink = setContext((_, {headers}) => {
+
+    //Leer el storage almacenado
+    const token = localStorage.getItem('token'); 
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '' 
+        }
+    }
+}); 
+
+const client = new ApolloClient({
+    connectToDevTools: true, 
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink)
+});
+
+export default client; 
